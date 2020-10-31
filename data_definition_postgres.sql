84af7c67c1f79f30571544b4892ef5139dc6bf61
Anthony Liu <anthonyliuhao@icloud.com>
2020-10-31 11:40:35 -0700

    Add tables to the data definition file and add sample data Here are the tables added ethical_problem ingredient_ethical_problem ingredient_alternative

--- data_definition_postgres.sql
+++ data_definition_postgres.sql
@@ -1,7 +1,14 @@
 DROP TABLE IF EXISTS test_table;
+
+-- First drop tables that depend on the other tables
+DROP TABLE IF EXISTS recipe_ingredient;
+DROP TABLE IF EXISTS ingredient_ethical_problem;
+DROP TABLE IF EXISTS ingredient_alternative;
+
+-- Then drop tables that are depended on
 DROP TABLE IF EXISTS ingredient;
 DROP TABLE IF EXISTS recipe;
-DROP TABLE IF EXISTS recipe_ingredient;
+DROP TABLE IF EXISTS ethical_problem;
 
 CREATE TABLE test_table (
     id SERIAL PRIMARY KEY NOT NULL,
@@ -18,15 +25,19 @@ CREATE TABLE ingredient (
 );
 
 INSERT INTO ingredient (name) VALUES
-    ('tomato'),
-    ('chicken broth'),
-    ('garlic'),
-    ('thyme'),
-    ('rosemary'),
-    ('black pepper'),
-    ('olive oil'),
-    ('pine nut'),
-    ('basil');
+    ('tomato'),             -- 1
+    ('chicken broth'),      -- 2
+    ('garlic'),             -- 3
+    ('thyme'),              -- 4
+    ('rosemary'),           -- 5
+    ('black pepper'),       -- 6
+    ('olive oil'),          -- 7
+    ('pine nut'),           -- 8
+    ('basil'),              -- 9
+    ('avocado'),            -- 10
+    ('onion'),              -- 11
+    ('heavy cream'),        -- 12
+    ('cashew cream');       -- 13
 
 CREATE TABLE recipe (
     id SERIAL PRIMARY KEY NOT NULL,
@@ -35,7 +46,9 @@ CREATE TABLE recipe (
 
 INSERT INTO recipe (name) VALUES
     ('Tomato Soup'),
-    ('Pesto');
+    ('Pesto'),
+    ('Guacamole');
+
 
 CREATE TABLE recipe_ingredient (
     recipe_id INT,
@@ -46,14 +59,54 @@ CREATE TABLE recipe_ingredient (
 );
 
 INSERT INTO recipe_ingredient (recipe_id, ingredient_id) VALUES
+    -- Tomato Soup
     (1, 1),
     (1, 2),
     (1, 3),
     (1, 4),
     (1, 5),
     (1, 6),
+    (1, 13), -- heavy cream
+    -- Pesto
     (2, 3),
     (2, 6),
     (2, 7),
     (2, 8),
-    (2, 9);
+    (2, 9),
+    -- Guacamole
+    (3, 10), -- Avocado
+    (3, 1), -- Tomato
+    (3, 3), -- Garlic
+    (3, 11); -- Onion
+
+create table ethical_problem (
+    id SERIAL PRIMARY KEY NOT NULL,
+    title text
+);
+
+insert into ethical_problem (title) values
+    ('deforestation'),
+    ('carbon emission');
+
+create table ingredient_ethical_problem (
+    ingredient_id INT,
+    problem_id INT,
+    PRIMARY KEY (ingredient_id, problem_id),
+    FOREIGN KEY (ingredient_id) REFERENCES ingredient (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (problem_id) REFERENCES ethical_problem (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+insert into ingredient_ethical_problem values
+    (10, 1), -- Avocado and deforestation
+    (13, 2); -- Heavy cream and carbon emission
+
+create table ingredient_alternative (
+    ingredient_id INT,
+    alternative_id INT,
+    PRIMARY KEY (ingredient_id, alternative_id),
+    FOREIGN KEY (ingredient_id) REFERENCES ingredient (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (alternative_id) REFERENCES ingredient (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+insert into ingredient_alternative (ingredient_id, alternative_id) values
+    (12, 13); -- Heavy cream and cashew cream
