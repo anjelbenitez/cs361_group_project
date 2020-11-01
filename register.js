2cc4fc76add408eacc1ddbc2b6576de7661e0855
Alex Lo <alexlo493@gmail.com>
2020-11-01 10:09:03 -0800

    added clearing of errors/values after close

--- register.js
+++ register.js
@@ -4,6 +4,36 @@ function displayRegisterForm() {
 
 function closeRegisterForm() {
     document.querySelector('.register_model_container').style.display = 'none';
+
+    let first_name = document.getElementById('first_name');
+    let last_name = document.getElementById('last_name');
+    let email = document.getElementById('email');
+    let username = document.getElementById('username');
+    let password = document.getElementById('password');
+    let confirm_password = document.getElementById('confirm_password');
+
+    // clear field values after closing
+    first_name.value = "";
+    last_name.value = "";
+    email.value = "";
+    username.value = "";
+    password.value = "";
+    confirm_password.value = "";
+
+    // clear error messages
+    let input_list = [];
+    input_list.push(first_name);
+    input_list.push(last_name);
+    input_list.push(email);
+    input_list.push(username);
+    input_list.push(password);
+    input_list.push(confirm_password);
+    console.log(input_list);
+    for (i=0; i<input_list.length; i++) {
+        if (input_list[i].nextElementSibling.childElementCount != 0){
+            input_list[i].nextElementSibling.children[0].remove();
+        }
+    }
 }
 
 function signUpAttempt() {
@@ -13,7 +43,7 @@ function signUpAttempt() {
     let username = document.getElementById('username');
     let password = document.getElementById('password');
     let confirm_password = document.getElementById('confirm_password');
-    let signUpForm = new SignUpForm(first_name, last_name, email, username, password, confirm_password)
+    let signUpForm = new SignUpForm(first_name, last_name, email, username, password, confirm_password);
     console.log("signUpform", signUpForm); //remove this later
     let formValidated = validateSignUp(signUpForm);
     if (formValidated == true){
@@ -74,7 +104,6 @@ function emptyFieldsVerification(signUpFormObj) {
         }
         else {
             if (nodeElement.nextElementSibling.childElementCount != 0){
-                console.log(nodeElement.nextElementSibling.children[0]);
                 nodeElement.nextElementSibling.children[0].remove();
             }
         }
