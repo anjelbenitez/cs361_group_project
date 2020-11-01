311e9d204c915abac768662b1a68b3c9a67a9f16
Anthony Liu <anthonyliuhao@icloud.com>
2020-10-31 23:31:30 -0700

    Include Add buttons to let users add ingredients to a recipe

--- public/js/BuildRecipeViewController.js
+++ public/js/BuildRecipeViewController.js
@@ -1,6 +1,6 @@
 class BuildRecipeViewController {
   constructor() {
-
+    this.recipe_ingredients = {};
   }
 
   pageDidLoad() {
@@ -30,6 +30,28 @@ class BuildRecipeViewController {
         let name_cell = document.createElement('td');
         name_cell.textContent = ingredient_name;
         row.appendChild(name_cell);
+
+        // Add an "Add to recipe" button
+        let addButton = document.createElement('button');
+        addButton.textContent = "Add";
+
+        // When user clicks on the Add button, add it to the recipe table
+        addButton.addEventListener('click', function () {
+          let recipe_table_body = document.getElementById("recipe-table-body");
+          let row = document.createElement('tr');
+
+          let id_cell = document.createElement('td');
+          id_cell.textContent = ingredient_id;
+          row.appendChild(id_cell);
+
+          let name_cell = document.createElement('td');
+          name_cell.textContent = ingredient_name;
+          row.appendChild(name_cell);
+
+          recipe_table_body.appendChild(row);
+        });
+        // Append the add button to the ingredient row
+        row.appendChild(addButton);
 
         // Add the row
         ingredients_table_body.appendChild(row);
