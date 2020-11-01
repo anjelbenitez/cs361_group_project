bcf8f4df469c8588662523de86bef2b9dcaf51c6
Anthony Liu <anthonyliuhao@icloud.com>
2020-10-31 23:03:15 -0700

    Request all ingredients and populate them in a simple table

--- public/js/BuildRecipeViewController.js
+++ public/js/BuildRecipeViewController.js
@@ -1,3 +1,43 @@
-document.addEventListener('DOMContentLoaded', function (event) {
-  console.log("BuildRecipeViewController!");
-});
+class BuildRecipeViewController {
+  constructor() {
+
+  }
+
+  pageDidLoad() {
+    document.addEventListener('DOMContentLoaded', this.loadPageContent);
+  }
+
+  loadPageContent() {
+    console.log("BuildRecipeViewController loading page content");
+
+    let si = new ServerInteractor();
+    si.getAllIngredients(function (ingredients) {
+      console.log(ingredients);
+
+      let ingredients_table_body = document.getElementById("ingredients-table-body");
+
+      for (let i = 0; i < ingredients.length; i++) {
+        let row = document.createElement('tr');
+
+        // Add the ID cell
+        let ingredient_id = ingredients[i]['id'];
+        let id_cell = document.createElement('td');
+        id_cell.textContent = ingredient_id;
+        row.appendChild(id_cell);
+
+        // Add the name cell
+        let ingredient_name = ingredients[i]['name'];
+        let name_cell = document.createElement('td');
+        name_cell.textContent = ingredient_name;
+        row.appendChild(name_cell);
+
+        // Add the row
+        ingredients_table_body.appendChild(row);
+
+      }
+    });
+  }
+}
+
+let vc = new BuildRecipeViewController();
+vc.pageDidLoad();
