0617a6ba09a957ac85e6fb7eebbb3c0433d5ff2b
hermancai <30858461+hermancai@users.noreply.github.com>
2020-11-19 00:43:34 -0800

    Add button to add alternative to recipe

--- public/js/build/BuildRecipeViewController.js
+++ public/js/build/BuildRecipeViewController.js
@@ -37,7 +37,8 @@ class BuildRecipeViewController {
 
         // Use the BuildRecipeFunctionFactory to create an add ingredient function
         let ff = new BuildRecipeFunctionFactory();
-        let addFunction = ff.createAddIngredientFunction(ingredient_id, ingredient_name, this);
+        var self = this;
+        let addFunction = ff.createAddIngredientFunction(ingredient_id, ingredient_name, self);
 
         // When user clicks on the Add button, add it to the recipe table
         addButton.addEventListener('click', addFunction);
@@ -47,6 +48,7 @@ class BuildRecipeViewController {
         //Add an "Info" button
         let infoButton = document.createElement('button');
         infoButton.textContent = "Info";
+
         // When user clicks on Info button, ingredient info show ups asynchronously on page
         infoButton.addEventListener('click', function() {
           function myCallback(result) {
@@ -54,7 +56,35 @@ class BuildRecipeViewController {
             // Update Ingredient Info box on page async
             document.getElementById("ingredient-name").textContent = result.ingredient;
             document.getElementById("ingredient-ethics").textContent = result.problem;
-            document.getElementById("ingredient-alternatives").innerHTML = result.alternative.join("<br>");
+
+            // Populate alternatives table with names and buttons
+            let altTable = document.getElementById("alt-table");
+            altTable.innerHTML = "";
+            if (result.alternative != "None") {
+              for (i = 0; i < result.alternative.length; i++) {
+                let altRow = altTable.insertRow();
+
+                // Add a cell to hold alternative name
+                let altNameCell = altRow.insertCell();
+                altNameCell.appendChild(document.createTextNode(result.alternative[i]));
+
+                // Add a cell to hold 'Add' button
+                let altAddCell = altRow.insertCell();
+                let altAddButton = document.createElement("button");
+                let altAddFunction = ff.createAddIngredientFunction(result.alternative_id[i], result.alternative[i], self);
+                altAddButton.textContent = "Add";
+                altAddButton.addEventListener("click", altAddFunction);
+                altAddCell.appendChild(altAddButton);
+
+                // Add a cell to hold 'Replace' button
+                let altReplaceCell = altRow.insertCell();
+                let altReplaceButton = document.createElement("button");
+                altReplaceButton.textContent = "Replace";
+                altReplaceCell.appendChild(altReplaceButton);
+              }
+            } else {
+              altTable.innerHTML = "None";
+            }
           }
           si.getIngredientInfo(myCallback, ingredient_id);
         });
